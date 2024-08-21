71e4925f1aff387abb5c0d5c1b1365a3c101094a
aashwinxgarg <161035135+aashwinxgarg@users.noreply.github.com>
2024-08-21 20:18:01 +0530

    Update user.js

--- schema/models/user.js
+++ schema/models/user.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 import { buffer } from "stream/consumers";
 
 const userSchema = new mongoose.Schema({
-    name: {type:String, required:false},
+    name: {type:String, required:true},
     email:{type:String, required:true},
     password:{type:String, required:true},
-    gender: {type:String, required:false},
-    dob: {type:Date, required:false},
-    mobile: {type:Number, requiref:false},
-    address: {type:String, required:false},
-    pincode: {type:Number, required:false},
+    gender: {type:String, required:true},
+    dob: {type:Date},
+    mobile: {type:Number, requiref:true},
+    address: {type:String},
+    pincode: {type:Number, required:true},
     idProof: { data: buffer, type: String, required:false},
     designation: {type:String, required:true}
 })
