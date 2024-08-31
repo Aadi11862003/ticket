68b53e1b07ac29e64b0551e892af6a9aed616b2e
Aadi11862003 <aadibharadwaj3@gmail.com>
2024-08-31 16:13:39 +0530

    up

--- frontend/src/components/SignupCard.jsx
+++ frontend/src/components/SignupCard.jsx
@@ -63,7 +63,7 @@ const SignupPage = () => {
             <div className="flex justify-center">
               <Link to="/login">
                 <button className="overflow-hidden w-40 p-2 h-12 border border-black bg-gray-400 font-serif text-black hover:text-gray-600 rounded-lg text-xl font-bold cursor-pointer relative z-10 group">
-                  Sign Up
+                  SignUp
                 </button>
               </Link>
             </div>
